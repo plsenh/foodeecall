77651ecabae11709ed3e5a251ae118fc8797ae4d
PLS <plsenh@gmail.com>
2019-03-09 19:03:55 -0500

    pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Search from "./pages/Search";
 import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <Books />
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Books} />
+          <Route exact path="/Books" component={Books} />
+          <Route exact path="/books/:id" component={Detail} />
+          <Route component={NoMatch} />
+
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/search" component={Search} />
+
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- server.js
+++ server.js
@@ -1,24 +1,48 @@
 const express = require("express");
-
+const session = require("express-session");
+const passport = require("passport");
 const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+// const users = require("./routes/api/users");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+let PORT = process.env.PORT || 3001;
 
-// Define middleware here
+// ======================
+// Middleware
+
+// Express aoo
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Authentication - sessions track user login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// ======================
 // Add routes, both API and view
+// app.use("/api/users", users);
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/foodeecall");
+// ======================
+// Connect to MongoDB
+// const db = require("./config/keys").mongoURI;
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/foodeecall";
 
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+// ======================
 // Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
